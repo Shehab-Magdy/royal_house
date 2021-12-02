69d5de1b8d2b9325ee7f5a035094a2997dd4e086
Shehab Magdy El-Adl <cegres1@hotmail.com>
2021-12-02 14:05:57 +0200

    set config.py to read from config.json

--- royal/config.py
+++ royal/config.py
@@ -1,5 +1,10 @@
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
 class Config:
-    SECRET_KEY = 'bdbfdc3388f5befed4b1a5dbb3c51b8a'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     UPLOAD_FOLDER = 'static/items_images'
     MAX_CONTENT_PATH = 210000 # maximum size of file uploaded – in bytes	
